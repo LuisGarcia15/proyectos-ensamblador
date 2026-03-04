ab6bd48c88ea5c5ed2c6ce0505994cabea8b4116
Luis <luis200107@icloud.com>
2026-03-04 01:30:35 -0600

    Modificando booloader

--- bootloader-ejemplo/bootloader-ejemplo.asm
+++ bootloader-ejemplo/bootloader-ejemplo.asm
@@ -1,4 +1,15 @@
-; DIRECTIVAS
+; DIRECTIVAS -  Es una operación orientada al ensamblador que le indica 
+;el curso/guia/comportamiento que debe tomar las instrucciones y/o
+;ejecución del programa. NO SON EJECUTADAS POR EL CPU
+
+;INSTRUCCIONES - Es una operación orientada a ser ejecutada por el CPU, 
+;expresada en con un nombre simbolico. Representa un instrucción de una
+;arquitectura que puede ejecutar el CPU.
+
+;LABEL - Es una nombre simbolico para un bloque de código, que describe un 
+;lugar en programa más facil que la dirección de memoria. _, ., ? son suados
+;como simbolos de inicio de la etiqueta.
+
 ORG 0 ;Permite especificar la dirección origen donde NASM asume que el programa inicio.
 ;desde ahí pudes dirigir el movimiento del programa.
 BITS 16 ;Especifica si NASM debe generar código generado para correr en una arquitectura
@@ -42,28 +53,32 @@ mov ss, ax ; Reset STACK SEGMENT to 0
 mov sp, 0x7C00 ; Set STACK POINTER to 0x7C00
 
 ;Ajustar inicio del DATA SEGMENT a CODE SEGMENT
-push cs
-pop ds
+push cs ;Envia el valor de CODE SEGMENT al STACK
+pop ds ;Saca el valor del stack y lo COLOCA A DATA SEGMENT
 
-push cs
-pop es
+push cs ;Envia el valor de CODE SEGMENT al STACK
+pop es ;Saca el valor del stack y lo COLOCA A EXTERNAL SEGMENT
 
 cld ;Clear Direction Flag - Determina como un conjunto de bits seran movidos
-;de un lugar en la memoria a otro. CausandO que SOURCE INDEX Y DESTINAITION
+;de un lugar en la memoria a otro. Causando que SOURCE INDEX Y DESTINAITION
 ;INDEX aumenten incrementalmente
 
 mov si, msg ; offset de msg en SOURCE INDEX (registro para operaciones en cadena)
 
 .print:
-lodsb
-test al, al ; Operacion AND entre al y al guardando las flags de la ALU
-jz .done ; Si ZF contiene 1, salta a done
+;Load Single Byte: Toma el contenido de la dirección de memoria apuntada por el
+;registro Data Segment y Source Index. Y lo coloca en AL
+    lodsb
+    test al, al ; Operacion AND entre al y al guardando las flags de la ALU
+    ;Jump If Zero: Evalua la bandera Zero Flag, si contine 1, salta a .done
+    jz .done ; Si Zero Flag contiene 1, salta a done
 
 mov ah, 0x0E ; Función 0x0E de la INT 10h: "Teletype Output"
 int 0x10 ; Llamada a la interrupción de video del BIOS
-jmp .print ; Repite para el siguiente carácter
+jmp .print ; Repite para el siguiente carácter - "Brinca a label .print"
+
 .done:
-hlt ; Detiene la ejecucion y pone el CPU en estado inactivo de bajo consumo
+    hlt ; Detiene la ejecucion y pone el CPU en estado inactivo de bajo consumo
 
 
 ; ------ Data ------
