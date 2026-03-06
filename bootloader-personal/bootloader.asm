8274020731451beb7a02d5e4d7252758d94d88ea
Luis <luis200107@icloud.com>
2026-03-06 00:12:56 -0600

    Ok

--- bootloader-personal/bootloader.asm
+++ bootloader-personal/bootloader.asm
@@ -1,92 +1,88 @@
 ORG 0
 BITS 16
-; --------- Formato inicial exigido por el BIOS (https://wiki.osdev.org/index.php?title=FAT#Boot_Record) ---------
-_start:
-jmp short start; 2 bytes
-nop; 1 byte
 
-times 33 nop ; Espacio para el BCP (Bios Control Block)
+_start:
+jmp short start
+nop
+
+times 33 nop
 
 start:
-jmp 0x07C0:main; Ajusta el CS (Code Segment) a 0x07C0, IP a main
+jmp 0x07C0:main
 
 main:
-cli ; Clear Interrupts
-mov [boot_drive], dl    ; Guardamos el valor de la unidad inmediatamente
-;---
-; Imprimimos el valor de la unidad detectada
-    mov al, [boot_drive]
-    call print_hex
+cli
+mov [boot_drive], dl
 
-; --- Rutina para imprimir hex ---
-print_hex:
-    push ax
-    push ax
-    shr al, 4
-    call .nibble
-    pop ax
-    call .nibble
-    pop ax
-    ret
-.nibble:
-    and al, 0x0F
-    cmp al, 10
-    jl .num
-    add al, 7
-.num:
-    add al, '0'
-    mov ah, 0x0E
-    int 0x10
-    ret
-;---
+; imprimir unidad de arranque
+mov al, [boot_drive]
+call print_hex
 
-;31 Bytes
-mov cx, 0x1000 ; Get 0
-mov ah, 0x02        ; Función: para leer sectores
-mov al, 0x01           ; Cantidad de sectores a leer
-mov ch, 0x00           ; Número de cilindro en chs
-mov dh, 0x00           ; Número de cabeza en chs
-mov cl, 0x02           ; Número de sector en chs
+; ---- leer sector 2 ----
+mov cx, 0x1000
+mov ah, 0x02
+mov al, 0x01
+mov ch, 0x00
+mov dh, 0x00
+mov cl, 0x02
 mov dl, [boot_drive]
 
-;ES:BX -> Buffer e Memoria. ES:Segmento BX:Offset
-mov es, cx      ; Coloca la dirección del segmento
-mov bx, 0x0000      ;Coloca la dirección del offset 
-int 0x13            ;llamada a int 13
+mov es, cx
+mov bx, 0x0000
+int 0x13
 
-xor ax, ax ; Get 0
-mov es, ax ;ES to 0
-mov ds, ax ; Reset data segment to 0
-; mov es, ax ; Reset extra segment (heap) to 0
-mov ss, ax ; Reset stack segment to 0
-mov sp, 0x7C00 ; Set stack pointer to 0x7C00
+jc .error_handler
+mov si, msg_success
+jmp .print_setup
 
-; Ajustar inicio del DS a CS
+.error_handler:
+mov si, msg_error
+
+.print_setup:
+
+; DS debe apuntar al mismo segmento del código
 push cs
 pop ds
 
-push cs
-pop es
-
 cld
 
-mov ax, 0x1000
-mov ds, ax          ; Ahora DS = 0x1000
-mov si, 0x0000      ; Offset inicial de la data (el comienzo del buffer)
-
 .print:
-    lodsb
-    test al, al ; Operacion AND entre al y al guardando las flags de la ALU
-    jz .done ; Si ZF contiene 1, salta a done
+lodsb
+test al, al
+jz .done
 
-    mov ah, 0x0E ; Función 0x0E de la INT 10h: "Teletype Output"
-    int 0x10 ; Llamada a la interrupción de video del BIOS
-    jmp .print ; Repite para el siguiente carácter
+mov ah, 0x0E
+int 0x10
+jmp .print
+
 .done:
-    hlt ; Detiene la ejecucion y pone el CPU en estado inactivo de bajo consumo
+hlt
 
+; --- imprimir byte en hex ---
+print_hex:
+push ax
+push ax
+shr al,4
+call .nibble
+pop ax
+call .nibble
+pop ax
+ret
 
-boot_drive: db 0
-; ------ Firma Boot Loader ------
+.nibble:
+and al,0x0F
+cmp al,10
+jl .num
+add al,7
+.num:
+add al,'0'
+mov ah,0x0E
+int 0x10
+ret
+
+boot_drive db 0
+msg_success db ' - Lectura exitosa!',0
+msg_error   db ' - Error de lectura!',0
+
 times 510-($-$$) db 0
 dw 0xAA55
